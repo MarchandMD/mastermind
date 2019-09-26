9f54290a36fa490bd5b231fc8770ca7f93eeaf21
Michael Marchand <michaeldavidmarchand@gmail.com>
2019-09-25 22:38:53 -0600

    tweaking the Computer#get_guess

--- lib/mastermind/computer.rb
+++ lib/mastermind/computer.rb
@@ -14,40 +14,46 @@ module Mastermind
 
     # def guess(hints, turn)
     #   computer_guess = get_guess(hints, turn)
-    #   print "#{computer_guess[0]}, #{computer_guess[1]}, #{computer_guess[2]}, #{computer_guess[3]}\n"
-    #   computer_guess
+    #    print "#{computer_guess[0]}, #{computer_guess[1]}, #{computer_guess[2]}, #{computer_guess[3]}\n"
+    #    computer_guess
     # end
 
-    # def get_guess(hints, turn)
-    #   computer_guess = []
+    def get_guess(hints = nil, turn = nil)
+      computer_guess = []
 
-    #   if @correct_colors.uniq.length != 4
-    #     case turn
-    #     when 1
-    #       4.times { computer_guess << @color_spectrum[@guess_set] }
+      if @correct_colors.uniq.length != 4
+             case turn
+             when 1
+              'turn 1'
+        #       4.times { computer_guess << @color_spectrum[@guess_set] }
 
-    #       @guess_set += 1
-    #     when (2..8)
-    #       4.times { computer_guess << @color_spectrum[@guess_set] }
+        #       @guess_set += 1
+             when (2..8)
+              'turn 2-8'
+        #       4.times { computer_guess << @color_spectrum[@guess_set] }
 
-    #       @correct_colors << @color_spectrum[@guess_set - 1] if color_is_correct(hints, turn)
+        #       @correct_colors << @color_spectrum[@guess_set - 1] if color_is_correct(hints, turn)
 
-    #       @guess_set += 1 if @guess_set < 8
-    #     when 9
-    #       @correct_colors << @color_spectrum[@guess_set - 1] if color_is_correct(hints, turn)
-    #       computer_guess = @correct_colors
-    #       @correct_colors = @correct_colors.shuffle
-    #     else
-    #       computer_guess = @correct_colors
-    #       @correct_colors = @correct_colors.shuffle
-    #     end
-    #   else
-    #     computer_guess = @correct_colors
-    #     @correct_colors = @correct_colors.shuffle
-    #   end
+        #       @guess_set += 1 if @guess_set < 8
+             when 9
+              'turn 9'
+        #       @correct_colors << @color_spectrum[@guess_set - 1] if color_is_correct(hints, turn)
+        #       computer_guess = @correct_colors
+        #       @correct_colors = @correct_colors.shuffle
+             else
+              'not 1, 2, 3, 4,5 ,6, 7, 8 or 9'
+        #       computer_guess = @correct_colors
+        #       @correct_colors = @correct_colors.shuffle
+             end
+        '@correct_colors.uniq.length is != 4'
+      else
+        #     computer_guess = @correct_colors
+        #     @correct_colors = @correct_colors.shuffle
+        '@correct_colors.uniq.length is == 4'
+      end
 
-    #   computer_guess
-    # end
+      computer_guess
+    end
 
     # def color_is_correct(hints, turn)
     #   hints[12 - turn + 1].colors[0] == "green"
--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -1,3 +1,38 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
+require "spec_helper"
+
+module Mastermind
+  RSpec.describe "Computer" do
+    context "#initialize" do
+      it 'sets the @color_spectrum instance variable' do
+        hal = Computer.new
+        expect(hal.color_spectrum).to eq(%w[red orange yellow green blue indigo violet])
+      end
+
+      it 'sets the @guess_set instance variable to 0' do
+        hal = Computer.new
+        expect(hal.guess_set).to eq(0)
+      end
+
+      it 'sets the @correct_colors instance variable to an empty Array' do
+        hal = Computer.new
+        expect(hal.correct_colors).to be_an(Array)
+        expect(hal.correct_colors.length).to eq(0)
+      end
+    end
+
+    context "#guess" do
+      it 'has yet to be written a unit test' do
+        # unit test here
+      end
+    end
+
+    context '#get_guess' do
+        it 'does something' do
+            # unit tests here
+        end
+    end
+
+  end
+end
