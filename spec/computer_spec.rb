114af112aac8f3a6173406a6707010f23eb1efd3
Michael Marchand <michaeldavidmarchand@gmail.com>
2019-09-28 13:56:23 -0600

    added details to Computer#guess

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -40,11 +40,25 @@ module Mastermind
     end
 
     context "#guess" do
-      it "doesn't do anything yet" do
+      it "returns a collection with 4 elements" do
         hint = Array.new(12, Hint.new("white", "white", "white", "white"))
-        hal = Computer.new
-        expect(hal.guess(hint,1)).to be_falsey
+        hal = Computer.new(1)
+        expect(hal.guess(hint,2).length).to eq(4)
       end
+
+      it "is a collection of colors" do
+        hint = Array.new(12, Hint.new("white", "white", "white", "white"))
+        hal = Computer.new(1)
+        hal.guess(hint,2).each do |el|
+          expect(el).to eq('red').or eq('green').or eq('yellow').or eq('blue').or eq('black').or eq('magneta').or eq('white')
+        end
+      end
+
+      # it 'does something' do
+      #   hint = Array.new(12, Hint.new("white", "white", "white", "white"))
+      #   hal = Computer.new(1)
+      #   expect { hal.guess(hint, 2) }.to output.to_stdout
+      # end
     end
 
     context "#get_guess" do
