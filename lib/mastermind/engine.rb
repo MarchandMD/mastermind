943b42be5aa8ed8e35d435a66040fb18d9c0be74
Michael Marchand <michaeldavidmarchand@gmail.com>
2019-09-28 21:38:10 -0600

    added a couple unit tests for Engine#new

---
+++ lib/mastermind/engine.rb
@@ -0,0 +1,135 @@
+# frozen_string_literal: true
+
+require_relative "gameboard.rb"
+require_relative "colorcode.rb"
+require_relative "hint.rb"
+require_relative "computer.rb"
+
+# responsible for the game engine
+module Mastermind
+  class Engine
+    attr_accessor :gameboard, :win, :turns, :color_spectrum, :player_mode, :computer
+
+    def initialize
+      @gameboard = Gameboard.new
+      @win = false
+      @turns = 1
+      @color_spectrum = %w[red green yellow blue black magenta cyan white]
+      @player_mode = true
+      @computer = Computer.new
+    end
+
+    def play
+      instructions
+      #   prompt_switch_mode
+      #   set_solution if @player_mode == false
+      #   start
+    end
+
+    def instructions
+      puts "  __  __            _                     _           _
+     |  \\/  | __ _  ___| |_  ___  _ _  _ __  (_) _ _   __| |
+     | |\\/| |/ _` |(_-<|  _|/ -_)| '_|| '  \\ | || ' \\ / _` |
+     |_|  |_|\\__,_|/__/ \\__|\\___||_|  |_|_|_||_||_||_|\\__,_|"
+      puts "_________________________________________________________\n\n"
+      puts "Guess the code to win!"
+      puts "The computer will select a code of four different colors in a specific order!"
+      puts "You have twelve guesses."
+      @gameboard.display
+      puts "_________________________________________________________\n\n"
+      puts "The left side is where your guesses will go."
+      puts "The right side will tell you how accurate you are: "
+      puts "green for every color in the correct position"
+      puts "red for every color in an incorrect position!"
+      puts "\nGood luck!"
+      puts "_________________________________________________________\n\n"
+    end
+
+    # def prompt_switch_mode
+    #   puts "Do you want to play or do you want the computer to play?"
+    #   print "Say \"computer\" or \"me\".\n> "
+    #   mode = gets.chomp
+    #   until mode == "computer" || mode == "me"
+    #     print "\nI didn't quite understand that. Say \"computer\" or \"me.\"\n> "
+    #     mode = gets.chomp
+    #   end
+    #   puts ""
+    #   @player_mode = false if mode == "computer"
+    # end
+
+    # def set_solution
+    #   puts "What would you like your solution to be?"
+    #   puts "\nType four colors separated by spaces."
+    #   print "Your choices are: red, green, yellow, blue, black, magenta, cyan, and white.\n> "
+    #   solution = get_player_guess
+    #   @gameboard.solution = Colorcode.new(solution[0], solution[1], solution[2], solution[3])
+    # end
+
+    # def start
+    #   make_guesses
+    #   turns > 12 ? lose : win
+    # end
+
+    # def make_guesses
+    #   while @win == false && @turns < 13
+    #     prompt_guess
+    #     guess = @player_mode ? get_player_guess : get_computer_guess
+    #     add_guess(guess)
+    #     @win = true if gameboard.guesses[12 - @turns].colors == gameboard.solution.colors
+    #     @turns += 1 if @win == false
+    #   end
+    # end
+
+    # def prompt_guess
+    #   puts @player_mode ? "What is your ##{@turns} guess?" : "\nComputer, make your ##{@turns} guess."
+    #   puts "Type four colors separated by spaces."
+    #   print "Your choices are: red, green, yellow, blue, black, magenta, cyan, and white.\n> "
+    # end
+
+    # def get_player_guess
+    #   1.times do
+    #     guess = gets.chomp.downcase.split(" ")
+
+    #     if guess.length != 4
+    #       print "\nYou entered the wrong amount of colors! Try again:\n> "
+    #       redo
+    #     end
+
+    #     if !@color_spectrum.include?(guess[0]) || !@color_spectrum.include?(guess[1]) || !@color_spectrum.include?(guess[2]) || !@color_spectrum.include?(guess[3])
+    #       print "\nYou can only enter the colors specified! Try again:\n> "
+    #       redo
+    #     end
+
+    #     if @player_mode == false
+    #       if guess.uniq.length != 4
+    #         print "\nYou must have different colors for the solution. Try again:\n> "
+    #         redo
+    #       end
+    #     end
+
+    #     return guess
+    #   end
+    # end
+
+    # def get_computer_guess
+    #   @computer.guess(@gameboard.hints, @turns)
+    # end
+
+    # def add_guess(guess)
+    #   gameboard.guesses[12 - @turns] = Colorcode.new(guess[0], guess[1], guess[2], guess[3])
+    #   gameboard.refresh(12 - @turns)
+    # end
+
+    # def lose
+    #   puts @player_mode ? "You have failed to crack the puzzle! It's okay; you're just not smart." : "No... I have failed."
+    #   puts "The solution was #{@gameboard.solution.colors}."
+    # end
+
+    # def win
+    #   puts @player_mode ? "\nYou have solved the code! Amazing!" : "\nI have solved the code, you measly human. Next I will destroy you!"
+    # end
+  end
+end
+
+#game = Mastermind::Engine.new
+#game.play
---
+++ spec/engine_spec.rb
@@ -0,0 +1,103 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Mastermind
+  RSpec.describe Engine do
+    context "#initialize" do
+      it "creates a Gameboard object" do
+        new_engine = Engine.new
+        expect(new_engine.gameboard).to be_a(Gameboard)
+      end
+
+      it 'sets win to false' do
+        new_engine = Engine.new
+        expect(new_engine.win).to be_falsey
+      end
+
+      it 'starts at turn 1' do
+        engine = Engine.new
+        expect(engine.turns).to eq(1)
+      end
+
+      it 'has a color spectrum attribute of the possible colors' do
+        engine = Engine.new
+        engine.color_spectrum.each do |el|
+          expect(el).to eq('red').or eq('green').or eq('yellow').or eq('blue').or eq('black').or eq('magenta').or eq('cyan').or eq('white')
+        end
+      end
+
+      it 'defaults to player mode' do
+        engine = Engine.new
+        expect(engine.player_mode).to be_truthy
+      end
+
+      it 'creats a Computer object' do
+        engine = Engine.new
+        expect(engine.computer).to be_a(Computer)
+      end
+    end
+
+    context "#play" do
+      it "starts to play" do
+        # unit tests here
+      end
+    end
+    context "#instructions" do
+      it "provides instructions" do
+        engine = Engine.new
+        expect{ engine.instructions }.to output.to_stdout
+      end
+    end
+    context "#prompt_switch_mode" do
+      it "prompts to switch modes" do
+        # unit tests here
+      end
+    end
+    context "#set_solution" do
+      it "sets the solution" do
+        # unit tests here
+      end
+    end
+    context "#start" do
+      it "starts to start" do
+        # unit tests here
+      end
+    end
+    context "#make_guesses" do
+      it "makes guesses" do
+        # unit tests here
+      end
+    end
+    context "#prompt_guesses" do
+      it "prompts for a guess" do
+        # unit tests here
+      end
+    end
+    context "#get_player_guess" do
+      it "gets the players guess" do
+        # unit tests here
+      end
+    end
+    context "#get_computer_guess" do
+      it "gets the computers guess" do
+        # unit tests here
+      end
+    end
+    context "#add_guess" do
+      it "adds a guess" do
+        # unit tests here
+      end
+    end
+    context "#lose" do
+      it "loses" do
+        # unit tests here
+      end
+    end
+    context "#win" do
+      it "wins" do
+        # unit tests here
+      end
+    end
+  end
+end
