d2bd28487811bc54a9dbd1c696d89054f6e8ebc6
Michael Marchand <michaeldavidmarchand@gmail.com>
2019-09-30 00:15:22 -0600

    re-factored colorcode object

--- lib/mastermind/colorcode.rb
+++ lib/mastermind/colorcode.rb
@@ -3,19 +3,35 @@
 module Mastermind
   # a necessary comment
   class Colorcode
-    attr_accessor :colors, :selected_colors
+    attr_accessor :colors
 
-    def initialize(color1 = random, color2 = random, color3 = random, color4 = random)
-      @colors = [color1, color2, color3, color4]
+    def initialize(*args)
+      @colors = if args.length == 4
+                  [args[0], args[1], args[2], args[3]]
+                else
+                  color_spectrum.shuffle.pop(4)
+                end
     end
 
-    def random
-      color_spectrum = %w[red green yellow blue black magenta cyan white]
-      random = color_spectrum.sample
-      @selected_colors ||= []
-      random = color_spectrum.sample while @selected_colors.include?(random)
-      @selected_colors << random
-      random
+    private
+
+    def color_spectrum
+      %w[red orange yellow green blue indigo violet]
     end
+
+    # attr_accessor :colors, :selected_colors
+
+    # def initialize(color1 = random, color2 = random, color3 = random, color4 = random)
+    #   @colors = [color1, color2, color3, color4]
+    # end
+
+    # def random
+    #   color_spectrum = %w[red green yellow blue black magenta cyan white]
+    #   random = color_spectrum.sample
+    #   @selected_colors ||= []
+    #   random = color_spectrum.sample while @selected_colors.include?(random)
+    #   @selected_colors << random
+    #   random
+    # end
   end
 end
